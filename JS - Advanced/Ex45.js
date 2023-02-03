6a7d2b5a478d5a388d0e20c526ec7dd9e4d327e2
Karam Guliyev <karamguliyev10@gmail.com>
2023-02-04 03:00:22 +0400

    Ex45 - Done

---
+++ JS - Advanced/Ex45.js
@@ -0,0 +1,9 @@
+// Perform a code refactoring by using the Spread Operator.
+// The output of the console.log must be the same.
+
+function sum(...nums) {
+  return nums.reduce((a, b) => a + b);
+}
+
+const numbers = [1, 2, 3];
+console.log(sum(numbers[0], numbers[1], numbers[2]));
