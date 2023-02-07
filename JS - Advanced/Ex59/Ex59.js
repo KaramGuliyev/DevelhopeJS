9c19e313cc4613c47b8d9af503be7e29888a7c81
Karam Guliyev <karamguliyev10@gmail.com>
2023-02-07 13:50:10 +0400

    Ex59 - Done

---
+++ JS - Advanced/Ex59/Ex59.js
@@ -0,0 +1,8 @@
+// Get the value of the firstName text input field and print it in the console.
+
+console.log("ss");
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let age = document.getElementById("age");
+
+console.log(firstName.value);
