36ae38d7d385123b9ffce4ecf6d2fc91caa8323f
Karam Guliyev <karamguliyev10@gmail.com>
2023-02-07 14:04:17 +0400

    Ex61 - Done / JS-Advanced Done

---
+++ JS - Advanced/Ex61/Ex61.js
@@ -0,0 +1,10 @@
+// Get the value of the firstName text input field and print it in the console.
+
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+let age = document.getElementById("age");
+
+console.log(
+`Name : ${firstName.value}
+ Last Name : ${lastName.value}
+ Age : ${age.value}`);
